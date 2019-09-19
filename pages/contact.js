ad1dd6ac30bb0a5baa04475b6226c2e067b92030
pavlosvos <pavlos@vos.gr>
2019-09-19 14:21:01 +0200

    Contact js added email as mailto:link

--- pages/contact.js
+++ pages/contact.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Layout from '../src/layout/Page';
+import Link from '../src/components/Link';
 import SectionTitle from '../src/components/SectionTitle';
 
 export default function Contact() {
@@ -7,7 +8,9 @@ export default function Contact() {
     <Layout>
       <SectionTitle primary="let’s talk about you!" secondary="That’s enough about me," />
       <p> If you want to chat please contact me at</p>
-      <p>pavlos (at) vos (dot) gr</p>
+      <Link href="mailto:pavlos@vos.gr" as={`mailto:pavlos@vos.gr`}>
+        <a>pavlos@vos.gr</a>
+      </Link>
     </Layout>
   );
 }
