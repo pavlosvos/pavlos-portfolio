59b135c5c86f208b692cf1829d723dd78e59843f
pavlosvos <pavlos@vos.gr>
2019-09-19 11:30:12 +0200

    Temporary email added to contact js component

--- pages/contact.js
+++ pages/contact.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Layout from '../src/layout/Page';
+import SectionTitle from '../src/components/SectionTitle';
 
 export default function Contact() {
   return (
     <Layout>
-      <p>This is the contact page</p>
+      <SectionTitle primary="let’s talk about you!" secondary="That’s enough about me," />
+      <p> If you want to chat please contact me at</p>
+      <p>pavlos (at) vos (dot) gr</p>
     </Layout>
   );
 }
