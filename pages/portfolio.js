22c8c54b70698602f8d0de50931b645af8a18cd5
pavlosvos <pavlos@vos.gr>
2019-10-22 13:21:49 +0200

    Portfolio locked icon added

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import Layout from '../src/layout/Page';
+import Markdown from '../src/components/Markdown';
+import SectionTitle from '../src/components/SectionTitle';
+import Typography from '../src/components/Typography';
+
+import content from './portfolio.md';
 
 export default function Portfolio() {
   return (
     <Layout containerProps={{ style: { height: '100vh' } }}>
+       <SectionTitle
+        primary="The nature of this work is confidential."
+        secondary="UX Design work for Accenture & Fjord and more"
+      />
+      <Markdown>{content}</Markdown>
       <iframe
         src="https://pavlosvos.myportfolio.com/"
         width="100%"
---
+++ pages/portfolio.md
@@ -0,0 +1,25 @@
+
+
+
+<br/><br/>
+
+
+
+<img src="static/locked.png" alt="Portfolio locked icon" style="width:200px;"/>
+I am ** not able to show it online**.
+<br/><br/><br/><br/>
+
+
+
+
+Please contact me for a offline discussion of my portfolio. 
+
+Below you can see some old work 
+
+
+<br/><br/><br/><br/>
+<br/><br/><br/><br/>
+<br/><br/><br/><br/>
+<br/><br/><br/><br/>
+<br/><br/><br/><br/>
+
